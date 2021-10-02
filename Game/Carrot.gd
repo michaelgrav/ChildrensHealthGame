1acdac4b47303dbc391741216f8a8a84f2d5477e
Michael Graves <mdgrav@protonmail.com>
2021-10-01 23:27:12 -0500

    Update

--- Game/Carrot.gd
+++ Game/Carrot.gd
@@ -1,8 +1,6 @@
 extends RigidBody2D
 signal hit
 
-export var min_speed = 150 # Minimum speed
-export var max_speed = 250 # Maximum speed
 export var speed = 5000
 var direction = Vector2()
 
@@ -13,12 +11,6 @@ func _ready():
 
 func _process(delta):
 	var velocity = speed * delta * direction
-	move_and_slide(velocity)
-	
-	if get_slide_count() > 0:
-		var collision = get_slide_collision(0)
-		if collision != null:
-			direction = direction.bounce(collision.normal)
 
 func _on_Carrot_body_entered(body):
 	pass # Replace with function body.
