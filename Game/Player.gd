23ffb4f782d7f58a95a499bb6431ba873dcceb6e
Michael Graves <mdgrav@protonmail.com>
2021-10-01 22:19:25 -0500

    Created carrot scene and update player sprite

--- Game/Player.gd
+++ Game/Player.gd
@@ -3,5 +3,20 @@ extends Area2D
 export var speed = 400 # How fast the player will move
 var screen_size# Size of the game window
 
+# Called when a node enters the scene tree
 func _ready():
 	screen_size = get_viewport_rect().size
+
+# Called every frame
+func _process(delta):
+	var velocity = Vector2() # Player's movement vector
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
