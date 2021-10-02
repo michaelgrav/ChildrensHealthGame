4c7ce4e4bd42bd40e1db254b987034c928517d9c
Michael Graves <mdgrav@protonmail.com>
2021-10-02 01:11:25 -0500

    Bouncing now works properly

--- Game/Carrot.gd
+++ Game/Carrot.gd
@@ -1,15 +1,14 @@
-extends RigidBody2D
+extends KinematicBody2D
 signal hit
 
 export var speed = 50
-var direction = Vector2()
+var direction = Vector2(200, 200)
 
-func _ready():
-	apply_impulse(Vector2(), Vector2(1,1).normalized() * speed)
-	set_bounce(1)
 
-func _process(delta):
-	var velocity = speed * delta * direction
+func _physics_process(delta):
+	var collison_info = move_and_collide(direction * delta)
+	if collison_info:
+		direction = direction.bounce(collison_info.normal)
 
 func _on_Carrot_body_entered(body):
 	pass # Replace with function body.
