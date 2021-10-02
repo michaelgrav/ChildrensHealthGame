f2d6157c718ebcf0c95ac126f3c6b6061456aa41
Ryan Gaulding <82967219+BigDaddyBlue42@users.noreply.github.com>
2021-10-02 08:50:34 -0500

    update

--- Game/Brick.gd
+++ Game/Brick.gd
@@ -8,10 +8,11 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-
+func rand():
+	pass
+	
 
 func _on_Area2D_area_entered(area):
 	queue_free()
--- Game/Main.gd
+++ Game/Main.gd
@@ -1,3 +1,18 @@
 extends Node2D
-
 var score
+var scence = preload("res://Brick.tscn")
+
+
+
+func _physics_process(delta):
+	pass
+	
+	
+func _ready():
+	var instance = scence.instance()
+	instance.rand()
+	add_child(instance)
+
+
+func generate():
+	print("Generating")
