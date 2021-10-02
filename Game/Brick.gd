55efd413fe145037b06f42c3fbe60db45b6f690a
Ryan Gaulding <82967219+BigDaddyBlue42@users.noreply.github.com>
2021-10-02 00:45:46 -0500

    brick go bye bye

---
+++ Game/Brick.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_Area2D_area_entered(area):
+	queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	print("HELLO,WORLD")
+	print(body)
+	if body.is_in_group("ball"):
+		print("BALLLL")
+		queue_free()
