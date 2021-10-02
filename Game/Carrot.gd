1ddca2d43b54c8c6beda0e1b76286b7fae42bcd1
Michael Graves <mdgrav@protonmail.com>
2021-10-02 08:48:54 -0500

    Sound effects

--- Game/Carrot.gd
+++ Game/Carrot.gd
@@ -9,6 +9,7 @@ func _physics_process(delta):
 	var collison_info = move_and_collide(direction * delta)
 	if collison_info:
 		direction = direction.bounce(collison_info.normal)
+		$AudioStreamPlayer.play()
 
 func _on_Carrot_body_entered(body):
 	pass # Replace with function body.
